81c9bbb67b25d143f321374d0dafe320d79878e7
Rafael Carvalho <rafaeldecarvalhodasilva01@yahoo.com.br>
2026-05-29 23:25:24 -0300

    chore: remove firebase e inicia configuração supabase

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,38 +1,8 @@
+/// <reference types="vite/client" />
 interface ImportMetaEnv {
-    readonly VITE_REACT_APP_API_KEY: string;
-    readonly VITE_REACT_APP_authDomain: string;
-    readonly VITE_REACT_APP_projectId: string;
-    readonly VITE_REACT_APP_storageBucket: string;
-    readonly VITE_REACT_APP_messagingSenderId: string;
-    readonly VITE_REACT_APP_appId: string;
-    readonly VITE_REACT_APP_measurementId: string;
-    // Adicione mais variáveis de ambiente conforme necessário
+  VITE_SUPABASE_PUBLISHABLE_KEY: string;
+  VITE_SUPABASE_URL: string;
 }
-
 interface ImportMeta {
-    readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
-
-declare module '*.png' {
-    const value: string;
-    export = value;
-  }
-  
-  declare module '*.jpg' {
-    const value: string;
-    export = value;
-  }
-  
-  declare module '*.jpeg' {
-    const value: string;
-    export = value;
-  }
-  
-  declare module '*.gif' {
-    const value: string;
-    export = value;
-  }
-  
-  declare module 'file-saver';
-
-  
