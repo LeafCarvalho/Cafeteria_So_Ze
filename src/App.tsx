4a830f846416cb8e90c2d47e5167a8a04f03036f
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2026-07-25 15:47:05 -0300

    fix: Restaura scroll ao navegar entre páginas (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CartProvider } from "./Context/CartContext";
 import Router from "./Router/Router";
+import { ScrollRestoration } from "@/components/ScrollRestoration/ScrollRestoration";
 
 const App: FC = () => (
   <CartProvider>
     <HashRouter>
+      <ScrollRestoration />
       <Router />
     </HashRouter>
   </CartProvider>
 );
 
 export default App;
-
