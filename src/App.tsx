3e6110f3ad5fda69bb9add3a9e067ce53b06786d
Rafael Carvalho <rafaeldecarvalhodasilva01@yahoo.com.br>
2026-07-26 13:46:51 -0300

    fix: Aprimora interações mobile e feedback de cópia

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { HashRouter } from "react-router-dom";
+import { Slide, ToastContainer } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import { CartProvider } from "./Context/CartContext";
 import Router from "./Router/Router";
 import { ScrollRestoration } from "@/components/ScrollRestoration/ScrollRestoration";
@@ -10,6 +12,16 @@ const App: FC = () => (
     <HashRouter>
       <ScrollRestoration />
       <Router />
+      <ToastContainer
+        autoClose={3000}
+        closeOnClick
+        draggable={false}
+        hideProgressBar
+        limit={1}
+        position="bottom-right"
+        theme="dark"
+        transition={Slide}
+      />
     </HashRouter>
   </CartProvider>
 );
